395f333e2e9e207bb385245ab00888285999886c
BastionCO <davidzhengxd@gmail.com>
2024-05-02 16:14:04 -0400

    add macd & ema data

--- Data Collection/twelvedata_fetchdata.py
+++ Data Collection/twelvedata_fetchdata.py
@@ -1,16 +1,17 @@
 from twelvedata import TDClient
 
-# api key
+# api key stored on local machine and is not pushed to git
 with open("twelvedata_apikey") as f:
     key = f.read()
 
 # api initiate
 td = TDClient(apikey=key)
 
-# symbol: stock symbol
-# interval: interval between datapoints
-# outputsize: n most recent datapoints
-ts = td.time_series(symbol="AAPL", interval="1day", outputsize=30).as_pandas()
+# tdclient paramters
+# - symbol: stock symbol
+# - interval: interval between datapoints
+# - outputsize: n most recent datapoints
+ts = td.time_series(symbol="AAPL", interval="1day", outputsize=30).with_macd().with_ema().as_pandas()
 
-# output as csv
-ts.to_csv('out.csv', index=False)
+# output stock data as csv
+ts.to_csv('out.csv', index=True)
